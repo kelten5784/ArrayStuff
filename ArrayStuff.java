5dda6b7abf83120f1894e39211b462d87cff9f3d
Casey Devet <casey.devet@sccdsb.net>
2020-10-21 12:03:00 -0400

    Add files via upload

---
+++ ArrayStuff.java
@@ -0,0 +1,54 @@
+/*
+ * Programmer:
+ * Date:
+ * Purpose:
+ */
+
+import java.util.Scanner;
+
+public class ArrayStuff {
+    public static void main (String[] args) {
+        // Create the input scanner
+        Scanner input = new Scanner(System.in);
+
+        // PART A
+
+        // TODO: Create integer array called "ages" with length 5.
+
+
+        // TODO: Change the 3rd element of "ages" to be 18
+
+
+        // TODO: Change the first element of "ages" to be an integer entered by the user
+        System.err.println("Enter an integer:");
+
+
+        // TODO: Change the last element of "ages" to be the first element squared
+
+
+        // TODO: Print all of the elements of "ages", one per line
+
+
+
+        // PART B
+
+        // TODO: Create AND initialize an array called "planets" that holds the
+        // names of the planets in order from closest to the sun to farthest.
+        // NOTE: Pluto is NOT a planet.
+
+
+        // TODO: Print the first letter of the 7th planet from the sun
+
+
+        // TODO: Change the name of our planet to be a line of text that the user inputs
+        System.err.println("What do you want to rename Earth to be?");
+
+
+        // TODO: Replace the first planet with the planet at index 2, but with all
+        // upper case letters.
+
+
+        // TODO: Print all of the elements of "planets" in reverse order.
+
+    }
+}
